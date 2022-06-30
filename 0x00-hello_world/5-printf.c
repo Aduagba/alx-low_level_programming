2212fad4c5d1b36681ab30c6140fcfc8abc3c0a3
aduagba <abdullahiaduagba@gmail.com>
2022-06-30 10:40:31 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
 #include <studio.h>
 int main(void)
 {
-prinf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
