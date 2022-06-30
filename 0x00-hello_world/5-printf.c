db0883fdc889207ae9dc19d5600097230a1d068c
aduagba <abdullahiaduagba@gmail.com>
2022-06-30 10:58:20 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 *main - entry poin
 *Return: 0 when successful
 */
-
 #include <studio.h>
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
