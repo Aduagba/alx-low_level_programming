1605b18d2d2b2a02106d339a7056a3177b4d309d
aduagba <abdullahiaduagba@gmail.com>
2022-06-30 11:36:17 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 /**
-*main – this function prints out a message
-*Return: the function returns a value of 0 when it runs successfully
-*/
-#include <studio.h>
-Int main(void)
+ * main - the main entry
+ * Return: return 0 if program run successfully
+ */
+#include <stdio.h>
+int main(void)
 {
-puts(“\”Programming is like building a multilingual puzzle”);
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
