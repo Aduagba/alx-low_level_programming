2c9a4a6a69ca7977b7af902ceb900b367a2bc781
aduagba <abdullahiaduagba@gmail.com>
2022-07-01 14:25:20 -0700

    Base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: always 0
+ */
+int main(void)
+{
+char alpha = 'a'
+int no = 0;
+while (no < 10)
+{
+putchar(no = '0');
+no++;
+}
+while (alpha <= 'f')
+{
+putchar(alpha);
+alpha++;
+}
+putchar(10);
+return (0);
+}
