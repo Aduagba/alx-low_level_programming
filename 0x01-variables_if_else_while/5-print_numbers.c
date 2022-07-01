21c446b65f96052bb92f83641574662e7fde6ac9
aduagba <abdullahiaduagba@gmail.com>
2022-07-01 13:40:07 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+int no = 0;
+while (no < 10)
+{
+printf("%d", no);
+no++;
+}
+putchar(10);
+return (0);
+}
