766351dcd66d39e3fd4932b4bc3e2a77486a77a7
aduagba <abdullahiaduagba@gmail.com>
2022-07-01 11:09:48 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 always
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int ch;
+	for (ch = 97; ch <= 122; ch++)
+	putchar(ch);
+	putchar(10); /* this is an ascii code for new line */
+
+return (0);
+}
