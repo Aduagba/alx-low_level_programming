70fb332e185de8f26891da9596da4986643d080f
aduagba <abdullahiaduagba@gmail.com>
2022-07-05 07:37:48 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabets in lowercases ten times.
+ * Return: 0 - SUCCESS
+ */
+
+void print_alphabet_x10(void)
+
+{
+char alpha;
+int no = 0;
+while (no < 10)
+{
+alpha = 'a';
+while (alpha <= 'z')
+}
+_putchar(alpha);
+alpha++;
+_putchar(10);
+no++;
+}
