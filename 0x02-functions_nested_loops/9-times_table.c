ad3543ca2090559cb7ba6357c6224b63a6a20c2f
aduagba <abdullahiaduagba@gmail.com>
2022-07-05 11:34:57 -0700

    trial

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,10 @@
+#include "main.h"
 /**
  * main - Entry point
  * Return: always 0 (success)
  */
-#include "main.h"
-void times_table(void);
+
+void times_table(void)
 {
 int num, mult, prod;
 for (num = 0; num <= 9; num++)
