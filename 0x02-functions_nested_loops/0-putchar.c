53a65d84d8fcbe659bb0d0e6f980a632b8ab83bb
aduagba <abdullahiaduagba@gmail.com>
2022-07-05 06:42:18 -0700

    Putchar 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Write a program that prints _putchar
+ * Return: 0 always - SUCCESS
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar(10);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Non empty README.md
