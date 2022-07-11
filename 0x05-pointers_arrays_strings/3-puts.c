1ce6e1f058f34a93fe873e9e5ae912c78836b57e
aduagba <abdullahiaduagba@gmail.com>
2022-07-11 10:49:55 -0700

    Puts funtion

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - puts function
+ * @str: _puts parameter
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
