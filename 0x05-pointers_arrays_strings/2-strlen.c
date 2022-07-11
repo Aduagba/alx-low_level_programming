923072c408a1add334764a298fc8cf8cc095593a
aduagba <abdullahiaduagba@gmail.com>
2022-07-11 10:36:47 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - length of a string
+ * @s: string pointer
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
