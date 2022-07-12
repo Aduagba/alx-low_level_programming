272a9a79f8f48f9fb2ace0bcba6e90ba3381ce41
aduagba <abdullahiaduagba@gmail.com>
2022-07-12 01:32:38 -0700

    Put a string function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * _puts - prints a string
- * @str: the string
- * Return: the length of the string
+ * _puts - puts a string
+ * @str: string function
+ * Return: length of the string
  */
 void _puts(char *str)
 {
 	int i = 0;
-while (str[i] != '\0')
-{
-	_putchar(str[i]);
-	i++;
-}
-_putchar('\n');
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
