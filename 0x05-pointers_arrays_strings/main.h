d6ab95643d682825d38f19c292d70b878369df03
aduagba <abdullahiaduagba@gmail.com>
2022-07-12 04:05:47 -0700

    t3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,5 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif /* MAIN_H*/
