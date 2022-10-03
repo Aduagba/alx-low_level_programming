7310d7456fed136ffd4025c9466d71b2e66966c0
Aduagba <abdullahiaduagba@gmail.com>
2022-10-03 13:20:58 -0700

    Dynamic Library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/mozaicdb/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so"
+wget -P /tmp -q "https://github.com/Aduagba/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so"
 export LD_PRELOAD="/tmp/libtest.so"
