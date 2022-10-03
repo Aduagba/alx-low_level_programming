aee1e1820d979360642cd92e76e99985634ec2dc
Aduagba <abdullahiaduagba@gmail.com>
2022-10-03 14:46:24 -0700

    all on Gaad

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aduagba/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp -q "https://github.com/aduagba/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so"
+export LD_PRELOAD="/tmp/iwin.so"
